ff23ce6b9009e155d82f9036a5beaeb8d5ca756f
Anewar <mohamedattandadamala@esp.sn>
2024-11-22 10:41:06 +0000

    Update Etudiant.cs

--- UsingCollectionsDemo/Etudiant.cs
+++ UsingCollectionsDemo/Etudiant.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace UsageCollections
 {
-   public class Étudiant
+   public class Etudiant
     {
         public string Nom { get; set; }
         public double NoteCC { get; set; }
